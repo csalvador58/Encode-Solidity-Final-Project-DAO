2859daa2f03ed4406b03e0a4f8a90dc0963c5f7c
Chris Salvador <csalvador58@gmail.com>
2023-04-03 23:27:12 -0700

    setup alchemy web3

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,48 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, useAccount, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { infuraProvider } from 'wagmi/providers/infura';
+import MainLayout from "../layout/mainLayout";
+import { useRouter } from "next/router";
+
+const { chains, provider } = configureChains(
+  [sepolia],
+  [infuraProvider({ apiKey: process.env.INFURA_API_KEY as string })]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Diploma Guild",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export { WagmiConfig, RainbowKitProvider };
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const account = useAccount({
+    onConnect({ address, connector, isReconnected }) {
+      if (!isReconnected) router.reload();
+    },
+  });
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+      >
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
